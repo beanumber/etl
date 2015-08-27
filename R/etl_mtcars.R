32cdb19e2d3ef21cc4cd781e530ae1bfd5cadafb
Ben Baumer <ben.baumer@gmail.com>
2015-08-27 09:44:04 -0400

    fixed documentation

--- R/etl_mtcars.R
+++ R/etl_mtcars.R
@@ -5,8 +5,9 @@
 #'  \item{con}{an objection of class \code{\link[DBI]{DBIConnection-class}}}
 #'  \item{dir}{the directory where the raw data is stored}
 #'  \item{init}{has the database been initialized?}
-#'  \item{push}{was the push successful?}
-#' }
+#'  \item{push}{a vector a messages from \code{\link[DBI]{dbWriteTable}}}
+#'  \item{files}{a list of files that have been downloaded}
+#'  }
 #' @export
 etl <- function()
 
